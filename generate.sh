37d6eb3c4149b03bc8a035b7ddd1f2751a8ac446
JeffBoermans <jeffboermans1@gmail.com>
2022-02-25 16:40:22 +0100

    sh file fix

--- generate.sh
+++ generate.sh
@@ -1,1 +1,1 @@
-java -jar src/generated/antlr-4.9.3-complete.jar -o bob -Dlanguage=Python3 Input/MyGrammar.g4 -visitor
+java -jar src/generated/antlr-4.9.3-complete.jar -o src/generated -Dlanguage=Python3 Input/MyGrammar.g4 -visitor
