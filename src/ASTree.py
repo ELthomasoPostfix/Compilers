ca42603565756c8693168809b7c88cad9e750339
JeffBoermans <jeffboermans1@gmail.com>
2022-02-26 19:56:18 +0100

    Added Preorder traversal and visual representation of ASTree.py

--- src/ASTree.py
+++ src/ASTree.py
@@ -4,15 +4,34 @@ class ASTree:
         self.children = []
         self.name = name
 
-
-    def toDot(self):
-        pass
-
-    def inorderTraverse(self, progress):
+    def preorderTraverse(self, progress, layer):
         if len(self.children) == 0:
-            progress.append([self.name, self.value])
+            progress.append([self.name, self.value, layer])
             return progress
         else:
+            progress.append([self.name, self.value, layer])
             for child in self.children:
-                pass
+                if len(child.children) != 0:
+                    child.preorderTraverse(progress, layer+1)
+                else:
+                    progress.append([child.name, child.value, layer+1])
+                # break
+        return progress
+
+    def toDot(self):
+        file = open("AST.dot", "w")
+        file.write("digraph AST {" + '\n')
+        traverse = self.preorderTraverse([], 0)
+        while len(traverse) != 0:
+            root = traverse[0]
+            del traverse[0]
+            for j in range(len(traverse)):
+                if traverse[j][2] == root[2]:
+                    break
+                if root[2] + 1 == traverse[j][2]:
+                    file.write('\t' + '"' + root[0] + "|" + str(root[1]) + '"' +
+                               "->" + '"' + traverse[j][0] + "|" + str(traverse[j][1]) + '"' + '\n')
+
+        file.write("}")
+
 
