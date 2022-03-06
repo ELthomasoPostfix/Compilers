da66c2064bf199dde48547171a5401841a674323
elthomaso <GueutalThomas@outlook.com>
2022-03-06 17:08:43 +0100

    - main.py   Changed to call test runner instead. - testing dir   Should contain all test files.

--- main.py
+++ main.py
@@ -1,52 +1,14 @@
-from antlr4 import *
-from src.generated.MyGrammarParser import MyGrammarParser
-from src.generated.MyGrammarLexer import MyGrammarLexer
-from src.generated.MyGrammarListener import MyGrammarListener
-from src.ASTree.ASTree import ASTree
-from src.ASTreeListener import ASTreeListener
-from src.Visitor.OptimizationVisitor import OptimizationVisitor
-from src.CompilersUtils import coloredDef
+import unittest
+from testing.test_listener import ListenerTestCase
 
 
-class KeyPrinter(MyGrammarListener):
-    def enterExp(self, ctx):
-        print(f"Werner found a {coloredDef(ctx.getText())} (EXP)")
+def suite():
+    suite = unittest.TestSuite()
+    suite.addTest(ListenerTestCase('test_default_listener_behavior'))
+    return suite
 
-    def enterValueexp(self, ctx):
-        print(f"Werner found a {coloredDef(ctx.getText())} (VEXP)")
-
-    def enterBop(self, ctx):
-        print(f"Werner found a {coloredDef(ctx.getText())} (BOP)")
-
-    def enterValue(self, ctx):
-        print(f"Werner found a {coloredDef(ctx.getText())} (VAL)")
-
-
-
-
-
-def main():
-    input_stream = FileStream("Input/test.txt")
-    lexer: MyGrammarLexer = MyGrammarLexer(input_stream)
-    stream = CommonTokenStream(lexer)
-    parser: MyGrammarParser = MyGrammarParser(stream)
-    tree = parser.expression()
-
-    printer = KeyPrinter()
-    listener = ASTreeListener(
-        ASTree(name="root", value=None))  # TODO make the root of the CST var 'tree' the ASTree root instead
-    walker = ParseTreeWalker()
-    walker.walk(printer, tree)
-    walker.walk(listener, tree)
-    listener.root.toDot("beginTree.dot")
-
-    OListener = OptimizationVisitor()
-    listener.root.children[0].children[0].accept(OListener)
-
-    listener.root.toDot("endTree.dot")
-
-    print(tree.getText())
 
 
 if __name__ == '__main__':
-    main()
+    runner = unittest.TextTestRunner()
+    runner.run(suite())
---
+++ testing/test_listener.py
@@ -0,0 +1,9 @@
+import unittest
+
+
+class ListenerTestCase(unittest.TestCase):
+    def SetUp(self):
+        print("ListenerTestCase")
+
+    def test_default_listener_behavior(self):
+        print("default listener behavior")
