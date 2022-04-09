f931f752312228ec76a5b341f140440e245aecef
elthomaso <GueutalThomas@outlook.com>
2022-04-09 12:40:28 +0200

    Added LLVM visitor boilerplate

--- src/Visitor/LLVMVisitor.py
+++ src/Visitor/LLVMVisitor.py
@@ -1,5 +1,7 @@
+from src.Nodes.LiteralNodes import promote, coerce
 from src.Visitor.ASTreeVisitor import ASTreeVisitor
 from src.Nodes.ASTreeNode import *
+from src.CompilersUtils import coloredDef
 
 
 class LLVMVisitor(ASTreeVisitor):
